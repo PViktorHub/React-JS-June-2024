db8cf9e4fcc0ef495bbc8c09f7dd3fed59e1d86b
Viktor.Ponomarenko <viktor.ponomarenko@eleks.com>
2025-12-03 17:52:27 +0200

    Fix hydration

--- GapAnalysis/router-ssr1/src/routes/routes.tsx
+++ GapAnalysis/router-ssr1/src/routes/routes.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import App from '../shared/App.js';
-import Home from '../pages/Home.js';
-import About from '../pages/About.js';
-import { RouteObject } from 'react-router-dom';
+import React from "react";
+import App from "../shared/App.js";
+import Home from "../pages/Home.js";
+import About from "../pages/About.js";
+import { RouteObject } from "react-router-dom";
 
 export const routes: RouteObject[] = [
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'about', element: <About /> }
-    ]
-  }
+      { path: "about", element: <About /> },
+    ],
+  },
 ];
--- GapAnalysis/router-ssr1/src/server/entry-server.tsx
+++ GapAnalysis/router-ssr1/src/server/entry-server.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
-import { routes } from "../routes/routes.js"; // тут твої маршрути у вигляді масиву RouteObject
+import { routes } from "../routes/routes.js";
 
-export async function render(url: string) {
-  // Створюємо маршрутизатор у пам'яті для SSR
-  const memoryRouter = createMemoryRouter(routes, {
-    initialEntries: [url],
-  });
+export function render(url: string) {
+  const memoryRouter = createMemoryRouter(routes, { initialEntries: [url] });
 
   const html = renderToString(<RouterProvider router={memoryRouter} />);
   return { html };
